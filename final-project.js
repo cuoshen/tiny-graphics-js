3c7e17ecf161683978fa93829a78a3f118167c37
cuoshen <chairmantoad@gmail.com>
2021-03-16 04:15:47 +0800

    Testing lava shader

--- final-project.js
+++ final-project.js
@@ -23,11 +23,20 @@ export class Final extends Scene {
             phong: new Material(new Phong_Shader(),{
                 color: color(1.0,1.0,1.0,1.0),
                 ambient: .5, diffusivity: 0.1, specularity: 0.1}),
-            normal_mapped: new Material(
-            new CustomShader(), {
+            normal_mapped_wood: new Material(
+            new Normal_Mapped(), {
                 ambient: .5, diffusivity: 0.1, specularity: 0.1,
                 albedo: new Texture("assets/wood_color.jpg","LINEAR_MIPMAP_LINEAR"),
                 normal: new Texture("assets/wood_normal.jpg", "LINEAR_MIPMAP_LINEAR")
+                }
+            ),
+            bump_mapped_stone: new Material(
+            new Bump_Mapped(), {
+                ambient: .5, diffusivity: 0.1, specularity: 0.1,
+                albedo: new Texture("assets/rock_color.jpg","LINEAR_MIPMAP_LINEAR"),
+                normal: new Texture("assets/rock_normal.jpg", "LINEAR_MIPMAP_LINEAR"),
+                bump: new Texture("assets/rock_height.png", "LINEAR_MIPMAP_LINEAR"),
+                lava_color: color(1.0, 69/256, 0.0, 1.0)
                 }
             ),
         }
@@ -58,11 +67,11 @@ export class Final extends Scene {
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
 
         //Sphere
-        this.shapes.sphere.draw(context, program_state, model_transform, this.materials.normal_mapped);
+        this.shapes.cube.draw(context, program_state, model_transform, this.materials.bump_mapped_stone);
     }
 }
 
-class CustomShader extends Phong_Shader {
+class Normal_Mapped extends Phong_Shader {
 
     vertex_glsl_code() { 
         return this.shared_glsl_code() + `
@@ -91,7 +100,7 @@ class CustomShader extends Phong_Shader {
             void main(){
                 vec4 tex_color = texture2D( albedo, f_tex_coord);
 
-                vec4 normal_sample = texture2D(albedo, f_tex_coord);
+                vec4 normal_sample = texture2D(normal_map, f_tex_coord);
                 vec3 normal = normalize(normal_sample.xyz);
 
                 if( tex_color.w < .01 ) discard;
@@ -120,3 +129,79 @@ class CustomShader extends Phong_Shader {
     }
 }
 
+class Bump_Mapped extends Phong_Shader {
+
+    vertex_glsl_code() { 
+        return this.shared_glsl_code() + `
+        varying vec2 f_tex_coord;
+        attribute vec3 position, normal;
+        attribute vec2 texture_coord;
+        
+        uniform mat4 model_transform;
+        uniform mat4 projection_camera_model_transform;
+
+        void main()
+          {
+            gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
+            N = normalize( mat3( model_transform ) * normal / squared_scale);   // True normal
+            vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
+            f_tex_coord = texture_coord;
+          } ` ;
+    }
+
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D albedo;
+            uniform sampler2D normal_map;
+            uniform sampler2D bump;
+            uniform vec4 lava_color;
+            
+            void main(){
+                vec4 tex_color = texture2D( albedo, f_tex_coord);
+
+                vec4 normal_sample = texture2D(normal_map, f_tex_coord);
+                vec3 normal = normalize(normal_sample.xyz);
+
+                vec3 bumped = vertex_worldspace;
+                vec4 bump_sample = texture2D(bump, f_tex_coord);
+                bumped += bump_sample.xyz;
+
+                if( tex_color.w < .01 ) discard;
+                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                gl_FragColor.xyz += phong_model_lights( normalize( normal ), bumped );
+
+                float height = length(bump_sample.xyz);
+                if (height < 0.5) {
+                    gl_FragColor = mix(lava_color, vec4(0.0,0.0,0.0,1.0), height*2.0);
+                }
+        } `;
+    }
+
+    update_GPU( context, gpu_addresses, gpu_state, model_transform, material )
+    {             // update_GPU(): Add a little more to the base class's version of this method.                
+      super.update_GPU( context, gpu_addresses, gpu_state, model_transform, material );
+                                               
+      if( material.albedo && material.albedo.ready )
+      {
+        context.uniform1i( gpu_addresses.albedo, 0);
+                                  // For this draw, use the texture image from correct the GPU buffer:
+        material.albedo.activate(context, 0);
+      }
+
+      // Apply normal map
+      if ( material.normal && material.normal.ready )
+      {
+          context.uniform1i(gpu_addresses.normal_map, 1);
+          material.normal.activate(context, 1);
+      }
+
+      if (material.bump && material.bump.ready) 
+      {
+         context.uniform1i(gpu_addresses.bump, 2);
+         material.bump.activate(context, 2); 
+      }
+
+      context.uniform4fv(gpu_addresses.lava_color, material.lava_color);
+    }
+}
