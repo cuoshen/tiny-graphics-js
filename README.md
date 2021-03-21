e20f6f12a98ff87f01e121bc28fd1f45c56f1603
cuoshen <chairmantoad@gmail.com>
2021-03-22 00:00:57 +0800

    flow map proof of concept

--- README.md
+++ README.md
@@ -17,6 +17,8 @@ Zhengyang Liu 105114114
 
 GDC 2019, Shading the World of Anthem (https://www.youtube.com/watch?v=IjQWRjWZGn0)
 
+GDC 2018, Applying AAA Techniques on Mobile Games: Understanding Flow Maps and Its Applications (https://www.gdcvault.com/play/1025044/Applying-AAA-Techniques-on-Mobile)
+
 Rock texture (https://3dtextures.me/2020/07/07/stylized-rocks-001/)
 
 ## Environment setup
--- final-project.js
+++ final-project.js
@@ -46,6 +46,19 @@ export class Final extends Scene {
                 lava_threshold: 0.3
                 }
             ),
+            flowing_lava: new Material(
+            new Flow_Mapped(), {
+                ambient: 0.5, diffusivity: 1.0, specularity: 10,
+                albedo: new Texture("assets/rock_color.jpg","LINEAR_MIPMAP_LINEAR"),
+                normal: new Texture("assets/rock_normal.jpg", "LINEAR_MIPMAP_LINEAR"),
+                bump: new Texture("assets/rock_height.png", "LINEAR_MIPMAP_LINEAR"),
+                lava_color: color(1.0, 69/256, 0.0, 1.0),
+                lava_threshold: 0.3,
+                flowmap: new Texture("assets/flowmap.png", "LINEAR_MIPMAP_LINEAR"),
+                flow_speed: 1.0,
+                time: 0.0
+            }
+            )
         }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 10), vec3(0, 0, 0), vec3(0, 1, 0));
@@ -62,7 +75,7 @@ export class Final extends Scene {
     }
 
     interpolate_lava_color(t) {
-        let lambda = (1 + Math.cos(t));
+        let lambda = (1 + Math.cos(t*10));
         return (color(1.0, lambda * 40/256, 0, 1,0));
     }
 
@@ -86,7 +99,7 @@ export class Final extends Scene {
         if (this.lit){
             program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
         } else {
-            program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 0)];
+            program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1)];
         }
 
         //Draw
@@ -97,7 +110,7 @@ export class Final extends Scene {
         }
 
         if (this.hasLava) {
-            this.activeMaterial = this.materials.bump_mapped_lava.override({lava_color: this.current_lava_color});
+            this.activeMaterial = this.materials.flowing_lava.override({lava_color: this.current_lava_color, time: t});
         } else {
             this.activeMaterial = this.materials.normal_mapped_stone;
         }
@@ -245,5 +258,58 @@ class Bump_Mapped extends Phong_Shader {
 
       context.uniform4fv(gpu_addresses.lava_color, material.lava_color);
       context.uniform1f(gpu_addresses.lava_threshold, material.lava_threshold);
+    }
+}
+
+class Flow_Mapped extends Bump_Mapped {
+
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D albedo;
+            uniform sampler2D normal_map;
+            uniform sampler2D bump;
+            uniform sampler2D flow_map;
+            uniform vec4 lava_color;
+            uniform float lava_threshold;
+            uniform float time;
+            uniform float flow_speed;
+            
+            void main(){
+                vec2 flow_uv = f_tex_coord;
+                vec2 uv_displacement = texture2D(flow_map, f_tex_coord).xy; // Sample displacement from flow map
+                uv_displacement -= vec2(0.5, 0.5); // Convert flow range from [0,1] to [-0.5, 0.5]
+                flow_uv += uv_displacement * flow_speed * time; // Compute final uv used for all other texture
+
+                vec4 tex_color = texture2D( albedo, flow_uv);
+
+                vec4 normal_sample = texture2D(normal_map, flow_uv);
+                vec3 normal = normalize(normal_sample.xyz);
+
+                vec3 bumped = vertex_worldspace;
+                vec4 bump_sample = texture2D(bump, flow_uv);
+                bumped += N.xyz * bump_sample.x; // bump by true normal
+
+                if( tex_color.w < .01 ) discard;
+                gl_FragColor = vec4(tex_color.xyz * ambient, tex_color.w);
+                gl_FragColor.xyz += phong_model_lights( normalize( normal ), bumped );
+
+                float height = bump_sample.x;
+                if (height < lava_threshold) {
+                    gl_FragColor = mix(lava_color, vec4(0.0,0.0,0.0,1.0), height*(1.0/lava_threshold));
+                }
+        } `;
+    }
+    
+    update_GPU( context, gpu_addresses, gpu_state, model_transform, material )
+    {             // update_GPU(): Add a little more to the base class's version of this method.                
+      super.update_GPU( context, gpu_addresses, gpu_state, model_transform, material );
+      if (material.flowmap && material.flowmap.ready) 
+      {
+         context.uniform1i(gpu_addresses.flow_map, 3);
+         material.flowmap.activate(context, 3); 
+         context.uniform1f(gpu_addresses.flow_speed, material.flow_speed);
+         context.uniform1f(gpu_addresses.time, material.time);
+      }
     }
 }
